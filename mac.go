4f366006daac7355ea4a771f5367f6dde2b8f2ca
Chris Redford <credford@gmail.com>
2020-03-21 15:10:20 -0700

    system support

--- mac.go
+++ mac.go
@@ -8,10 +8,6 @@ import (
 	"strings"
 )
 
-//////////////////////////////////////////////
-/// System
-//////////////////////////////////////////////
-
 func Expanduser(path string) string {
 	usr, _ := user.Current()
 	dir := usr.HomeDir
@@ -20,10 +16,6 @@ func Expanduser(path string) string {
 	}
 	return path
 }
-
-//////////////////////////////////////////////
-/// RunApplescript
-//////////////////////////////////////////////
 
 // RunApplescript runs the ApplesScript contained in script, returning the output and any errors.
 func RunApplescript(script string) (string, error) {
@@ -37,4 +29,54 @@ func RunApplescript(script string) (string, error) {
 	}
 
 	return prettyOutput, nil
+}
+
+func GetFrontMostApplication() (string, error) {
+	script := `path to frontmost application as Unicode text`
+	path, err := TellSystemEvents(script)
+	if err != nil {
+		return path, err
+	}
+	nameSplit := strings.Split(path, ":")
+	name := nameSplit[len(nameSplit)-2]
+	appSplit := strings.Split(name, ".")
+	app := appSplit[len(appSplit)-2]
+
+	return app, err
+}
+
+func TellSystemEvents(script string) (string, error) {
+	out, err := Tell("System Events", script)
+	return out, err
+}
+
+func Tell(application string, commands ...string) (string, error) {
+	return RunApplescript(buildTell(application, commands...))
+}
+
+func wrapInQuotes(text string) string {
+	return "\"" + text + "\""
+}
+
+// Parse the Tell options and build the command
+func buildTell(application string, commands ...string) string {
+	application = wrapInQuotes(application)
+	args := []string{"tell application", application, "\n"}
+	for _, command := range commands {
+		args = append(args, command, "\n")
+	}
+	args = append(args, "end", "tell")
+	return build(args...)
+}
+
+func build(params ...string) string {
+	var validParams []string
+
+	for _, param := range params {
+		if param != "" {
+			validParams = append(validParams, param)
+		}
+	}
+
+	return strings.Join(validParams, " ")
 }
